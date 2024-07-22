0d882ac9d29775711dcac86b0d1db39bd87cdd15
jyhyunpark <122863609+yeochi1201@users.noreply.github.com>
2024-07-22 14:10:27 +0900

    StepMotor

--- TOPST/Library/Module/ULN2003_Library.py
+++ TOPST/Library/Module/ULN2003_Library.py
@@ -1,4 +1,5 @@
 from .. import GPIO_Library as gpio
+import time
 
 pins= []
 def set_driver(input_pins):
@@ -7,7 +8,6 @@ def set_driver(input_pins):
         gpio.set_direction(pin, "out")
         gpio.set_value(pin, 0)
         pins.append(pin)
-
 def set_location(input_pin, input_pin2 = -1):
     for pin in pins:
         if(pin == input_pin):
@@ -16,3 +16,16 @@ def set_location(input_pin, input_pin2 = -1):
             gpio.set_value(pin, 1)
         else:
             gpio.set_value(pin, 0)
+
+def rotation(step_count, direction):
+    step_counter = 0
+    for i in range(0,step_count):
+        if step_counter%2 ==0:
+                set_location(pins[step_counter//2])
+        else:
+            set_location(pins[step_counter//2],pins[(step_counter//2+1)%4])
+        if direction == True:
+            step_counter = (step_counter -1)%8
+        else:
+            step_counter = (step_counter +1)%8
+        time.sleep(0.01)
--- TOPST/Module/ULN2003_Controller.py
+++ TOPST/Module/ULN2003_Controller.py
@@ -1,15 +1,9 @@
 from ..Library.Module import ULN2003_Library as uln
-import time
-
 pins = [112, 113, 114, 121]
 
+step_count = 4096
+step_sleep = 0.002
+direction = True
 if __name__ == "__main__":
     uln.set_driver(pins)
-    i = 0
-    while(True):
-        for i in range(0,8):
-            if i%2 ==0:
-                uln.set_location(pins[i/2])
-            else:
-                uln.set_location(pins[i/2],pins[(i/2+1)%4])
-            time.sleep(1)
+    uln.rotation(step_count, direction)
